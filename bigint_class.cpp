8faf6e11345e52f21fae397f451e53e87958de78
Rezaul H Sagar <rezaulhsagar@gmail.com>
2017-08-25 00:45:52 +0600

    more operator overloading

--- bigint_class.cpp
+++ bigint_class.cpp
@@ -5,6 +5,15 @@ class BigInt{
 public:
     string s;
 
+    BigInt& operator = (string Int){
+        s = Int;
+    }
+
+    friend istream& operator >> (istream& in, BigInt& data){
+        cin>>data.s;
+        return in;
+    }
+
     friend ostream& operator << (ostream& out, const BigInt& data){
         cout<<data.s;
         return out;
